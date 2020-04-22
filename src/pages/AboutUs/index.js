8a4ced98208c49a3db753dbb5d56e8d419ee3756
curtis-clement <curtis.clement.nl@gmail.com>
2020-04-22 12:27:46 +0200

    added info to about us page

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import style from '../../CSS Modules/aboutus.module.css';
+import teampic from '../../images/teampic.jpg';
 
 export default function AboutUs() {
   return (
-    <div className={style.App}>
-      About Amsterdam Ultimate Club
+    <div className={style.body}>
+      <div>
+        <h1 className={style.header}>About Our Club</h1>
+        <hr className={style.line}/>
+      </div>
+
+      <div className={style.picdiv}>
+        <img className={style.teampic} src={teampic} alt='Team' />
+        <div className={style.infodiv}>
+          <h3>Welcome - We are glad you found us!</h3>
+          <p>Amsterdam Ultimate Club (Or AUC) is an ultimate frisbee club based in Amsterdam. Founded in 1991 and originally 
+            known as Crunch, AUC is the largest ultimate club in The Netherlands with more than 160 members of all age, gender,
+            origin, size, and level of play. Throughout the years we have grown from a sports club into a vibrant community within
+            this amazing and diverse city we call home.
+          </p>
+          <br/>
+          <p>Besides the largest, we're also one of the most successful clubs in the country! 
+            Each year, several of our teams compete at the national level, while our first teams also compete internationally. 
+            And to complete the bragging, our club also produces some of the country's brightest talents through its youth program.
+          </p>
+          <br/>
+          <p>
+            Apart from Dutch competition, AUC regularly organizes seasonal events such as City League, goaltimate, an annual beach tournament,
+            and all kinds of fun events in the city. We welcome playeres of all skill levels and aim to make sure everyone can enjoy
+            this amazing sport we love!
+          </p>
+        </div>
+      </div>
     </div>
   )
-}
+};
