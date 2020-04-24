0e9f0d10eb8a7d6e6a41c830136406fcd3dd2136
curtis-clement <curtis.clement.nl@gmail.com>
2020-04-24 09:41:04 +0200

    minor changes

--- src/pages/WhatIsUltimate/index.js
+++ src/pages/WhatIsUltimate/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import style from '../../CSS Modules/whatisultimate.module.css';
 
 export default function WhatIsUltimate() {
   return (
     <div>
-      What is Ultimate though?
+      <div>
+      <h1 className={style.header}>Current AUC Club Teams</h1>
+      </div>
+      <hr className={style.line}/>
     </div>
   )
 }
