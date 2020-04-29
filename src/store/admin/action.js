a9f55d0758468e3cc8307ab418bb9b6c51d48bb1
curtis-clement <curtis.clement.nl@gmail.com>
2020-04-29 22:16:35 +0200

    add player to team shows in database

--- src/store/admin/action.js
+++ src/store/admin/action.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import {apiUrl} from '../../config/constants';
 import {fetchAllTeams} from '../teams/action';
+import {
+  appLoading, 
+  appDoneLoading,
+  showMessageWithTimeout
+} from '../appState/actions';
 
 export const DELETE_TEAM = 'DELETE_TEAM';
 export const ADD_PLAYERTEAM = 'ADD_PLAYERTEAM'
@@ -28,10 +33,19 @@ const addPlayerTeam = (team, player) => ({
   payload: team, player
 })
 
-export const updatePlayerTeam = (teamId, playerId) => {
+export const updatePlayerTeam = (teamId, userId) => {
   return async (dispatch) => {
+    dispatch(appLoading)
     try {
       console.log('Dispatch is working')
+      const response = await axios.post(`${apiUrl}/teams/${teamId}/add/${userId}`,
+      {
+        teamId,
+        userId
+      });
+      dispatch(addPlayerTeam(response.data));
+      dispatch(showMessageWithTimeout('Success!', false, 'Player Added', 2000));
+      dispatch(appDoneLoading())
     } catch(error) {
       console.log(error.message)
     };
