b133ccd20eaed4c329812e8739f188effe9d1874
curtis-clement <curtis.clement.nl@gmail.com>
2020-05-09 23:02:59 +0200

    update fetch teams

--- src/store/teams/reducer.js
+++ src/store/teams/reducer.js
@@ -1,15 +1,16 @@
 import {ADD_NEW_TEAM} from '../teams/action';
 import {DELETE_TEAM} from '../teams/action';
+import {FETCH_ALL_TEAMS} from '../teams/action';
 
 const initialState = [];
 
 export default (state = initialState, action) => {
   switch(action.type) {
-    case 'FETCH_ALL_TEAMS':
+    case FETCH_ALL_TEAMS:
       return [...action.payload];
 
     case ADD_NEW_TEAM:
-      return []
+      return [...state, action.payload];
 
     case DELETE_TEAM:
       const teamId = action.payload;
